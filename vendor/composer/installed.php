19b6079ff135fe4e24302e248f75409e86f8d94a
piyojiro <dondon12don@yahoo.co.jp>
2021-04-29 20:52:29 +0900

    composer json 他　当初

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '947da5c7fcb42a8e275362d5385a8dc180dfeac0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '947da5c7fcb42a8e275362d5385a8dc180dfeac0',
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '7.0.0',
+      'version' => '7.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '210d87af3f5fa3ed7837c3c875e39e1e3caeb9b7',
+    ),
+  ),
+);
