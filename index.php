64bab875b8853553b117253968cbc4f48866ce92
piyojiro <dondon12don@yahoo.co.jp>
2021-04-30 17:24:54 +0900

    環境変数を入れた

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ooNXytK / brsAZLBJnVb8fzLwOAKJNGcsPXSeb8o / NNm7IDi65U / qakqgy1z3YH7bbEOhFc3g9W5gt7oVsOdZPgTYLA84 + GRX5kCfD8Nj2kIu0P6gh3WbhVG7');
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '64b84a919ba2fdd252f5c7ac3a60600d']);
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
