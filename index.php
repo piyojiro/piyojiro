33f47d218cdf6f561969dee7a30fb896f1e3c9a6
piyojiro <dondon12don@yahoo.co.jp>
2021-04-30 12:39:28 +0900

    変更した

--- index.php
+++ index.php
@@ -10,6 +10,10 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
+// POSTメソッドで渡される値を取得・表示
+$inputString = file_get_contents('php://input');
+error_log($inputString);
+
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
 // 不正であれば例外の内容を出力
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
